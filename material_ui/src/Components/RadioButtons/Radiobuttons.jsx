f152adb850ceea9b29ea484931627d51c47ca885
vinay <vinayambekar.b@gmail.com>
2024-04-14 09:34:09 +0530

    'radio_button'

--- material_ui/src/Components/RadioButtons/Radiobuttons.jsx
+++ material_ui/src/Components/RadioButtons/Radiobuttons.jsx
@@ -1,7 +1,30 @@
+import { Radio } from "@mui/material";
+import { useState } from "react";
+
 const Radiobuttons=()=>{
+    let [gender,setgender]=useState('male')
+    function testfunc(e){
+        console.log(e.target.value)
+        setgender(e.target.value)
+    }
     return(
         <>
         <h2>Radio buttons</h2>
+        <label>male</label>
+        <Radio value='male'
+        checked={gender=='male'}
+        onChange={testfunc}
+        ></Radio>
+        <label>female</label>
+        <Radio value='female'
+        checked={gender=='female'}
+        onChange={testfunc}>
+        </Radio>
+        <label>other</label>
+        <Radio value='other'
+        checked={gender=='other'}
+        onChange={testfunc}>
+        </Radio>
         </>
     )
 }
